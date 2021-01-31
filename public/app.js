253c8d64761b01c97cb6fccc43828bad052b57b6
Kassam Ali Asif <asif.k.dev@gmail.com>
2021-01-31 12:27:49 +0100

    feat: improvement & email implementation

--- public/app.js
+++ public/app.js
@@ -12,31 +12,95 @@ const signOutBtn = document.getElementById('signOutBtn');
 
 const userDetails = document.getElementById('userDetails');
 
-// Log in function : 
+// Section login with email 
+let authEmail = document.getElementById('authEmail');
+
+let register = document.getElementById('registerWithEmail');
+let registerSection = document.getElementById('registerSection');
+let registerForm = document.getElementById('registerForm');
+
+let signWithEmail = document.getElementById('signWithEmail');
+let loginSection = document.getElementById('loginSection');
+let loginForm = document.getElementById('loginForm');
+
+// Login with google : 
 const provider = new firebase.auth.GoogleAuthProvider();
 
-// click event with an existing method on firebase : 
 signedInBtn.onclick = () => auth.signInWithPopup(provider)
 
 // unlog : 
 signOutBtn.onclick = () => auth.signOut();
 
-// logic to see if auth : 
+// Register with an email : 
+register.addEventListener("click", () => {
+    registerSection.hidden = false;
+    loginSection.hidden = true ? loginSection.hidden = true : '';
+});
+
+registerForm.onsubmit = (e) => {
+    e.preventDefault();
+
+    let userEmail = document.getElementById('userEmail').value;
+    let userPassword = document.getElementById('userPassword').value;
+
+    //Create User with Email and Password
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  });
+};
+
+// Login with an email :
+signWithEmail.onclick = () => {
+    loginSection.hidden = false;
+    if(registerSection.hidden = true) {
+        registerSection.hidden = true;
+    }
+};
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginPassword = document.getElementById('loginPassword').value;
+
+    firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
+
+});
+
+
+// Set an authentication state observer and get user data of the user connected : 
 auth.onAuthStateChanged(user => {
     if(user) {
         console.log(user)
         whenSignedIn.hidden = false;
         whenSignedOut.hidden = true;
+        register.classList.add('hidden');
+        authEmail.classList.add('hidden');
+        let loggendWithEmail = user.email.substring(0, user.email.indexOf("@"));
+        let currentUser = user.displayName === null ? loggendWithEmail.charAt(0).toUpperCase() + loggendWithEmail.slice(1) : user.displayName;
         userDetails.innerHTML = `
-            <h3> Hello ${user.displayName}! </h3>
+            <h3> Hello ${currentUser}! </h3>
             <p>Your email adress is ${user.email}  </p>
         `
     } else {
         whenSignedIn.hidden = true;
         whenSignedOut.hidden = false;
+        register.classList.remove('hidden');
+        authEmail.classList.remove('hidden');
         userDetails.innerHTML = "";
     }
-})
+});
+
 
 // Database : 
 const db = firebase.firestore();
@@ -44,7 +108,7 @@ const db = firebase.firestore();
 const createThing = document.getElementById('createThing');
 const thingsList = document.getElementById('thingsList');
 const form = document.getElementById('form');
-const secondTwo = document.getElementById('secondTwo')
+const sectionDatabase = document.getElementById('sectionDatabase')
 
 // Reference to a database location : 
 let thingsRef;
@@ -57,7 +121,7 @@ auth.onAuthStateChanged(user => {
         // Database Reference
         thingsRef = db.collection('clients');
 
-        secondTwo.hidden = false; 
+        sectionDatabase.hidden = false; 
 
         form.onsubmit = (e) => {
             e.preventDefault();
@@ -91,34 +155,7 @@ auth.onAuthStateChanged(user => {
     } else {
         // Unsubscribe when the user signs out
         unsubscribe && unsubscribe();
-        secondTwo.hidden = true;
+        sectionDatabase.hidden = true;
     }
 });
-
-// Register 
-
-let register = document.getElementById('registerWithEmail');
-let registerForm = document.getElementById('registerForm');
-let authEmail = document.getElementById('authEmail');
-
-register.addEventListener("click", () => {
-    registerForm.hidden = false;
-});
-
-registerForm.onsubmit = (e) => {
-    e.preventDefault();
-
-    let userName = document.getElementById('userName').value;
-    let userEmail = document.getElementById('userEmail').value;
-    let userPassword = document.getElementById('userPassword').value;
-
-    //Create User with Email and Password
-    firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-  });
-}
 
